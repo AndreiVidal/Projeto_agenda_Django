821f50e855e13b700428ba44a3ec61c615c037d3
Andrei Godoi <andrei.godoi92@gmail.com>
2023-06-08 23:34:31 -0300

    change laguage "en"

--- Project/settings.py
+++ Project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "pt-br"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "America/Sao_Paulo"
 
